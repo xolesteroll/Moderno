7220e7e7f04367300347832ef1e304711518a7af
xolesteroll <xol2dem@gmail.com>
2020-08-19 18:26:03 +0300

    product-one page done

--- app/js/main.js
+++ app/js/main.js
@@ -42,6 +42,15 @@ $(function(){
 
     });
 
+    $('.product-one__tabs .tab').on('click', function(event) {
+        var id = $(this).attr('data-id');
+            $('.product-one__tabs').find('.tab-item').removeClass('active-tab').hide();
+            $('.product-one__tabs .tabs').find('.tab').removeClass('active');
+            $(this).addClass('active');
+            $('#'+id).addClass('active-tab').fadeIn();
+            return false;
+        });
+
     $('.menu__btn').on('click', function(){
         $('.menu__list').slideToggle();
     });
