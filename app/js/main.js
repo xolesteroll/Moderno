ceef8e422c7716ce0564f2dfa8d2e59ced29ede8
Anton <Iarovoi>
2020-08-25 14:17:16 +0300

    profiel page, upload- page, form styler

--- app/js/main.js
+++ app/js/main.js
@@ -51,6 +51,8 @@ $(function(){
             return false;
         });
 
+    $('input[type="file"], select').styler();
+
     $('.menu__btn').on('click', function(){
         $('.menu__list').slideToggle();
     });
--- gulpfile.js
+++ gulpfile.js
@@ -25,6 +25,8 @@ gulp.task('style', function(){
         'node_modules/slick-carousel/slick/slick.css',
         'node_modules/ion-rangeslider/css/ion.rangeSlider.css',
         'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.css',
+        'node_modules/jquery-form-styler/dist/jquery.formstyler.css',
+        'node_modules/jquery-form-styler/dist/jquery.formstyler.theme.css',
         'node_modules/magnific-popup/dist/magnific-popup.css'
     ])
         .pipe(concat('libs.min.css'))
@@ -39,6 +41,7 @@ gulp.task('script', function(){
         'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
         'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
         'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
+        'node_modules/jquery-form-styler/dist/jquery.formstyler.js',
         'node_modules/mixitup/dist/mixitup.js'
     ])
         .pipe(concat('libs.min.js'))
